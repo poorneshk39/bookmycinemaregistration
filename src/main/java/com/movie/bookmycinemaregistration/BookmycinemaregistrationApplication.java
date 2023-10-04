3fb0e9dc7c4fb463e618c248e82a0cadd2ddeb39
poorneshk39 <poorneshk39@gmail.com>
2023-10-05 02:19:50 +0530

    initial commit

---
+++ src/main/java/com/movie/bookmycinemaregistration/BookmycinemaregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.movie.bookmycinemaregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmycinemaregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmycinemaregistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie/bookmycinemaregistration/BookmycinemaregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.bookmycinemaregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmycinemaregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
